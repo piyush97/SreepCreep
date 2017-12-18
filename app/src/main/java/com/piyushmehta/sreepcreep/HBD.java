109a7dd2e6a21ace2f00dc9a5dc221fdcb183838
piyush97 <me@piyushmehta.com>
2017-12-18 21:46:01 +0530

    new changes

---
+++ app/src/main/java/com/piyushmehta/sreepcreep/HBD.java
@@ -0,0 +1,13 @@
+package com.piyushmehta.sreepcreep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HBD extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hbd);
+    }
+}
--- app/src/main/java/com/piyushmehta/sreepcreep/Images.java
+++ app/src/main/java/com/piyushmehta/sreepcreep/Images.java
@@ -1,8 +1,10 @@
 package com.piyushmehta.sreepcreep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ViewFlipper;
 
@@ -28,6 +30,10 @@ public class Images extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    public void another(MenuItem item) {
+        startActivity(new Intent(Images.this, HBD.class));
     }
 }
 
